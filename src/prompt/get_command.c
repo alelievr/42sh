2a1e57ce6890f2deab9c6cca767a5e0a1c95c1e1
fdaudre- <felix.daudre.vignier@gmail.com>
2015-03-18 17:56:02 +0100

    prompt basique

--- src/prompt/get_command.c
+++ src/prompt/get_command.c
@@ -6,7 +6,7 @@
 /*   By: alelievr <alelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/16 16:30:07 by alelievr          #+#    #+#             */
-/*   Updated: 2015/03/18 17:41:15 by fdaudre-         ###   ########.fr       */
+/*   Updated: 2015/03/18 17:55:35 by fdaudre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,17 @@
  * SUPPR
 */
 
+#define pr_move NULL
 static t_pr_code		g_pr_codes[] =
 {
-//	{4414235ull, pr_move}, // right
-//	{4479771ull, pr_move}, // left
+	{4414235ull, pr_move}, // right
+	{4479771ull, pr_move}, // left
+	{74982532143899ull, pr_move}, // ctrl + left
+	{73883020516123ull, pr_move}, // ctrl + left
+	{4741915ull, pr_move}, // begin
+	{1ull, pr_move}, // begin
+	{4610843ull, pr_move}, // end
+	{5ull, pr_move}, // end
 	{0, NULL}
 };
 
@@ -51,9 +58,11 @@ static void				pr_affbuff(t_prompt *d)
 {
 	ft_putstr(tgetstr("cr", NULL));
 	ft_putstr(tgetstr("cd", NULL));
-//	ft_printf("%s", tgetstr("cd", NULL));
+//ft_printf("[%llu]", d->key);
 	ft_printf("%{F}$> %{!F}", 123);
 	ft_putstr(d->buff);
+	ft_putstr(tgetstr("cr", NULL));
+	ft_printf("\033[%luC", 3 + d->index);
 }
 
 char					*get_command(void)
@@ -70,7 +79,6 @@ char					*get_command(void)
 		d.key = 0;
 		if ((read(0, &(d.key), 8) < 1) || (d.key == 4) || (d.key == 10))
 			break ;
-//ft_printf("[%llu]\n", d.key);
 		if (ft_isprint(d.key) && (d.key != '\t'))
 			pr_addchar(&d);
 		i = -1;
@@ -82,5 +90,5 @@ char					*get_command(void)
 			}
 	}
 	default_terminal_mode();
-	return (d.key == 4 ? NULL : (char *)d.buff);
+	return ((d.key == 4) && (!d.index) ? NULL : (char *)d.buff);
 }
