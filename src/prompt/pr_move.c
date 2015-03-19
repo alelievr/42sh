275c231184977911691d3dbcfe2f8531667b64e8
fdaudre- <felix.daudre.vignier@gmail.com>
2015-03-19 12:17:09 +0100

    prompt +ctrl move

--- src/prompt/pr_move.c
+++ src/prompt/pr_move.c
@@ -6,7 +6,7 @@
 /*   By: fdaudre- <fdaudre-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/19 11:38:48 by fdaudre-          #+#    #+#             */
-/*   Updated: 2015/03/19 12:11:04 by fdaudre-         ###   ########.fr       */
+/*   Updated: 2015/03/19 12:16:25 by fdaudre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,19 @@ void					pr_move(t_prompt *d)
 		d->index = 0;
 	else if ((d->key == PR_END) || (d->key == PR_C_E))
 		d->index = ft_strlen(d->buff);
-	else if (d->key == PR_S_LE)
+	else if ((d->key == PR_S_LE) && d->index)
 	{
+			d->index--;
 		while (d->index && (d->buff[d->index] == ' '))
 			d->index--;
 		while (d->index && (d->buff[d->index] != ' '))
 			d->index--;
+		if (d->buff[d->index] == ' ')
+			d->index++;
 	}
-	else if (d->key == PR_S_RI)
+	else if ((d->key == PR_S_RI) && (d->buff[d->index]))
 	{
+			d->index++;
 		while (d->buff[d->index] && (d->buff[d->index] != ' '))
 			d->index++;
 		while (d->buff[d->index] && (d->buff[d->index] == ' '))
