361028fb0704a6ff664a16e7694eb0a823a30ce3
alelievr <alelievr@student.42.fr>
2015-03-19 19:54:25 +0100

    src

--- src/operation/ft_error_right_redir.c
+++ src/operation/ft_error_right_redir.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_right_redir.c                                   :+:      :+:    :+:   */
+/*   ft_error_right_redir.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alelievr <alelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/03/18 16:21:56 by alelievr          #+#    #+#             */
-/*   Updated: 2015/03/19 19:36:15 by alelievr         ###   ########.fr       */
+/*   Created: 2015/03/19 19:43:51 by alelievr          #+#    #+#             */
+/*   Updated: 2015/03/19 19:51:21 by alelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_42sh.h"
 #include <fcntl.h>
 
-void			ft_right_redir(int n, char **files)
+void			ft_error_right_redir(int ac, char **av)
 {
 	int		fd;
 
-	if (n == 1)
+	if (ac == 1)
 	{
-		if ((fd = open(*files, O_CREAT | O_RDWR, 0644)) == -1)
+		if ((fd = open(*av, O_CREAT | O_RDWR, 0644)) == -1)
 			ft_putstr("Broken redirection !\n");
 		else
-			dup2(fd, 1);
+			dup2(fd, 2);
 	}
 	else
 		ft_putstr("Too many arguments for redirection !\n");
---
+++ src/prompt/pr_move.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pr_move.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdaudre- <fdaudre-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/03/19 11:38:48 by fdaudre-          #+#    #+#             */
+/*   Updated: 2015/03/19 12:16:25 by fdaudre-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_42sh.h"
+
+void					pr_move(t_prompt *d)
+{
+	if ((d->key == PR_RI) && (d->buff[d->index]))
+		d->index++;
+	else if ((d->key == PR_LE) && d->index)
+		d->index--;
+	else if ((d->key == PR_BEG) || (d->key == PR_C_A))
+		d->index = 0;
+	else if ((d->key == PR_END) || (d->key == PR_C_E))
+		d->index = ft_strlen(d->buff);
+	else if ((d->key == PR_S_LE) && d->index)
+	{
+			d->index--;
+		while (d->index && (d->buff[d->index] == ' '))
+			d->index--;
+		while (d->index && (d->buff[d->index] != ' '))
+			d->index--;
+		if (d->buff[d->index] == ' ')
+			d->index++;
+	}
+	else if ((d->key == PR_S_RI) && (d->buff[d->index]))
+	{
+			d->index++;
+		while (d->buff[d->index] && (d->buff[d->index] != ' '))
+			d->index++;
+		while (d->buff[d->index] && (d->buff[d->index] == ' '))
+			d->index++;
+	}
+}
