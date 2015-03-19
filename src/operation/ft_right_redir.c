caf09369f7ea644342f4f0910abb87f74e80eb60
alelievr <alelievr@student.42.fr>
2015-03-19 01:30:31 +0100

    blabla

--- src/operation/ft_right_redir.c
+++ src/operation/ft_right_redir.c
@@ -6,7 +6,7 @@
 /*   By: alelievr <alelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/18 16:21:56 by alelievr          #+#    #+#             */
-/*   Updated: 2015/03/18 18:53:47 by alelievr         ###   ########.fr       */
+/*   Updated: 2015/03/18 22:35:27 by alelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,15 @@ void			ft_right_redir(int n, char **files)
 				ft_putstr("Broken redirection !\n");
 			fd = dup(1);
 			dup2(filedes[i], fd);
+			ft_printf("duplicate filedes 1 to %i and duped in %i\n",
+					fd, filedes[i]);
 			n--;
 			i++;
 		}
 		if ((filedes[i] = open(*files, O_CREAT | O_RDWR, 0644)) == -1)
 			ft_putstr("Broken redirection !\n");
 		dup2(filedes[i], fd);
+		ft_printf("duped filedes %i in %i", fd, filedes[i]);
 	}
 	else if (n == 1)
 	{
--- src/prompt_test.c
+++ src/prompt_test.c
@@ -6,7 +6,7 @@
 /*   By: fdaudre- <fdaudre-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/18 15:39:32 by fdaudre-          #+#    #+#             */
-/*   Updated: 2015/03/18 18:42:30 by alelievr         ###   ########.fr       */
+/*   Updated: 2015/03/18 22:39:51 by alelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,15 @@ void					ft_prompt(void)
 		ret = read(0, buff, 0xF00 - 1);
 		buff[ret - 1] = 0;
 		ft_builtins(ft_strsplit(buff, ' '));
-		char	*v1[3] = {"t1", "t2"};
-		char	*v2 = "ls";
+		char	**v1 = (char **)malloc(sizeof(char *) * 3);
+		char	**v2 = (char **)malloc(sizeof(char *) * 3);
+		v1[0] = "t1";
+		v1[2] = "t2";
+		v1[3] = NULL;
+		v2[0] = "ls";
+		v2[1] = NULL;
 		op = create_op_redir(v1, 2, REDIR_R);
-		op->next = create_op_redir(&v2, 1, BIN);
+		op->next = create_op_redir(v2, 1, BIN);
 		execute_command(op);
 	}
 	(void)tmp;
