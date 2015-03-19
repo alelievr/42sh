962cc33132356cca0c7eb2eb0e4d2ce61212a158
alelievr <alelievr@student.42.fr>
2015-03-19 22:14:33 +0100

    nfew

--- src/execute_command.c
+++ src/execute_command.c
@@ -6,11 +6,12 @@
 /*   By: alelievr <alelievr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/18 17:09:04 by alelievr          #+#    #+#             */
-/*   Updated: 2015/03/18 18:56:48 by alelievr         ###   ########.fr       */
+/*   Updated: 2015/03/19 22:05:08 by alelievr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_42sh.h"
+#include <fcntl.h>
 
 void		(*op[11])(int, char **) = {
 							ft_pipe,
@@ -23,7 +24,7 @@ int			execute_command(t_operate *b)
 {
 	while (b)
 	{
-		ft_printf("%i\n", b->type);
+		ft_fprintf(open("/dev/tty", O_RDWR), "%i\n", b->type);
 		op[b->type](b->len, b->value);
 		b = b->next;
 	}
