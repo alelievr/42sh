4a163ed13a3102f325f2b4bf846798b129be02e3
Antoine LELIEVRE <alelievr@e1r7p14.42.fr>
2016-03-23 18:20:33 +0100

    added wildcard file match file

---
+++ src/preparser/globing_file.c
@@ -0,0 +1,224 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   globing_file.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alelievr <alelievr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/23 15:37:49 by alelievr          #+#    #+#             */
+/*   Updated: 2016/03/23 18:19:25 by alelievr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/syslimits.h>
+#include <sys/stat.h>
+
+typedef struct		s_mlist
+{
+	char			*str;
+	struct s_mlist	*next;
+}					t_mlist;
+
+static void		copy_next_regex(char **s, char *buff, size_t max)
+{
+	char	*ret;
+	size_t	i;
+
+	i = 0;
+	ret = *s;
+	while (ret[i] && ret[i] != '/' && i < max - 1)
+		*buff++ = ret[i++];
+	*buff = 0;
+	*s = ret + i + ((ret[i] == '/' && ret[i + 1]) ? 1 : 0);
+}
+
+static char		*concat_path(char *dirs, char *file)
+{
+	if (!*dirs)
+	{
+		strcpy(dirs, "./");
+		strlcat(dirs, file, PATH_MAX);
+		return (dirs);
+	}
+	if (dirs[strlen(dirs) - 1] != '/')
+		strlcat(dirs, "/", PATH_MAX);
+	strlcat(dirs, file, PATH_MAX);
+	return (dirs);
+}
+
+static t_mlist	*new_mlist(char *s)
+{
+	t_mlist	*ret;
+
+	if (!(ret = malloc(sizeof(t_mlist))))
+		exit(-1);
+	if (!(ret->str = malloc(sizeof(char) * PATH_MAX)))
+		exit(-1);
+	strcpy(ret->str, s);
+	ret->next = NULL;
+	return (ret);
+}
+
+static int		isdir(char *path)
+{
+	struct stat	st;
+
+	lstat(path, &st);
+	return (S_ISDIR(st.st_mode));
+}
+
+static int		cmd_has_wildcard(char *s)
+{
+	while (*s)
+	{
+		if (*s == '*' || *s == '?' || *s == '[')
+			return (1);
+		s++;
+	}
+	return (0);
+}
+
+static void		cmd_create_path_regex(char **s, char *oldpath, char *path, char *regex)
+{
+	strlcpy(path, oldpath, PATH_MAX);
+	printf("fullregex2 = %s\n", *s);
+	copy_next_regex(s, regex, 0xF00);
+	printf("oldpath = %s\n", oldpath);
+	printf("regex = %s\n", regex);
+	printf("path = %s\n", path);
+	while (!cmd_has_wildcard(regex) && *regex)
+	{
+		concat_path(path, regex);
+		printf("fullregex2 = %s\n", *s);
+		copy_next_regex(s, regex, 0xF00);
+		printf("fullregex2 = %s\n", *s);
+		printf("regex = %s\n", regex);
+		printf("path = %s\n", path);
+	}
+}
+
+static int		cmd_is_last_regex(char *r)
+{
+	while (*r && *r != '/' && *r != '*' && *r != '?' && *r != '[')
+		r++;
+	return (!*r);
+}
+
+int             cmd_globing_match(char *s1, char *s2);
+
+static void		cmd_list_add_file_if_matching(char *fname, char *fpath, char *regex, t_mlist **t, char *fullregex)
+{
+	char			buff[PATH_MAX];
+	t_mlist			*new;
+
+	if (*fname == '.' && *regex != '.')
+		return ;
+	strcpy(buff, fpath);
+	concat_path(buff, fname);
+	if (cmd_globing_match(fname, regex)) //match files with regex
+	{
+		if (cmd_is_last_regex(fullregex) || (!cmd_is_last_regex(fullregex) && isdir(buff)))
+		{
+			new = new_mlist(buff);
+			new->next = *t;
+			*t = new;
+		}
+	}
+}
+
+static void		cmd_add_list_to_list(t_mlist *t, t_mlist **b, char *end)
+{
+	t_mlist		*tmp;
+	t_mlist		*new;
+	char		buff[PATH_MAX];
+
+	while (t)
+	{
+		strlcpy(buff, t->str, PATH_MAX);
+		strlcat(buff, end, PATH_MAX);
+		printf("final match path = %s\n", buff);
+		if (access(buff, F_OK) == 0)
+		{
+			new = new_mlist(t->str);
+			new->next = *b;
+			*b = new;
+		}
+		t = t->next;
+	}
+}
+
+static void		cmd_match_patern_rec(char *s, char *p, t_mlist **b)
+{
+	char			*regex;
+	char			*path;
+	t_mlist			*tmp;
+	DIR				*dir;
+	struct dirent	*dd;
+
+	if (!(regex = malloc(sizeof(char) * 0xF00)))
+		exit(-1);
+	if (!(path = malloc(sizeof(char) * PATH_MAX)))
+		exit(-1);
+	cmd_create_path_regex(&s, p, path, regex);
+
+	tmp = NULL;
+	printf("matching regex withs files in dirs: %s\n", regex);
+	printf("fullregex = %s\n", s);
+	if (!(dir = opendir(path)))
+		return ;
+	while ((dd = readdir(dir)))
+		cmd_list_add_file_if_matching(dd->d_name, path, regex, &tmp, s);
+
+	///////////////////////////////////////////////////////////////
+	printf("file list:\n");
+	t_mlist	*tt = tmp;
+	while (tt)
+	{
+		printf("\t%s\n", tt->str);
+		tt = tt->next;
+	}
+	///////////////////////////////////////////////////////////////
+
+	printf("fullregex = %s\n", s);
+	if (cmd_is_last_regex(s))
+		cmd_add_list_to_list(tmp, b, s);
+	else
+		while (tmp)
+		{
+			cmd_match_patern_rec(s, tmp->str, b);
+			tmp = tmp->next;
+		}
+	tmp = NULL;
+	free(regex);
+}
+
+t_mlist			*cmd_match_patern_files(char *s)
+{
+	t_mlist		*tmp;
+
+	tmp = NULL;
+	cmd_match_patern_rec(s, "", &tmp);
+	return (tmp);
+}
+
+int			main(int ac, char **av)
+{
+	t_mlist			*tmp;
+
+	if (ac == 2)
+	{
+		tmp = cmd_match_patern_files(av[1]);
+		printf("returned file list:\n");
+		while (tmp)
+		{
+			printf("%s\n", tmp->str);
+			tmp = tmp->next;
+		}
+	}
+	return (0);
+}
